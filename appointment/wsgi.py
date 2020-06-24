5cfc3d0c5025a962c2c2d37776857eff63890029
Yashvi1730 <171220055@nitdelhi.ac.in>
2020-06-24 12:24:55 -0700

    my third commit

--- appointment/wsgi.py
+++ appointment/wsgi.py
@@ -16,5 +16,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'appointment.settings')
 
 application = get_wsgi_application()
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
+
