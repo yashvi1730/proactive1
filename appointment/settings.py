c15e5f6c051401766f05de64d99290fb8dafadb8
Yashvi1730 <171220055@nitdelhi.ac.in>
2020-06-25 05:37:04 -0700

    my fourth commit

--- appointment/settings.py
+++ appointment/settings.py
@@ -128,13 +128,3 @@ STATICFILES_DIRS=[
 ]
 
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage' 
