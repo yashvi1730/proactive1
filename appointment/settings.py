b179ad201522509addc1e6b87c23e4379a0bde5c
Yashvi1730 <171220055@nitdelhi.ac.in>
2020-06-25 08:23:56 -0700

    my first commit

--- appointment/settings.py
+++ appointment/settings.py
@@ -24,9 +24,9 @@ STATIC_DIR=os.path.join(BASE_DIR,'static')
 SECRET_KEY = '0@i)4cnbb9kc80n-_$z%)u)whg9-ht&*k$4!#3k6-g$7b%3s#!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
